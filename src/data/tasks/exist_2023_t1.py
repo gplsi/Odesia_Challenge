d0f49bdddf0fda6a2825c5bc913cfdf88c3436a4
mikelCanal <mikel.canal@ua.es>
2025-01-20 12:19:45 +0100

    upd task 1 prompt

--- src/data/tasks/exist_2023_t1.py
+++ src/data/tasks/exist_2023_t1.py
@@ -4,6 +4,12 @@ from src.preprocessing.exist_2023_t1 import get_counts
 
 
 class Exist2023T1PromptBuilder(TaskPromptBuilder):
+    def __init__(
+        self,
+        prompt_start="For this task, solve the following classification problem:\n",
+    ):
+        self.prompt_start = prompt_start
+
     def build(
         self,
         prompt_syntax: PromptSyntax,
@@ -17,12 +23,7 @@ class Exist2023T1PromptBuilder(TaskPromptBuilder):
         ]
         formated_answer = self.format_output(input) if answer else None
         prompt = prompt_syntax.build(self.format_input(input), "", formated_answer)
-        return (
-            "For this task, solve the following classification problem:\n"
-            + "\n".join(retrieved_prompts)
-            + "\n"
-            + prompt
-        )
+        return self.prompt_start + "\n".join(retrieved_prompts) + "\n" + prompt
 
     def format_input(self, entry):
         return entry["tweet"]
